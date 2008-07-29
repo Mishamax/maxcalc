13201393d5bb0b5002b2473d462f6bac38e5dfec
Michael.Maximov <Michael.Maximov@localhost>
2008-07-29 18:14:08 +0000

    Initial commit

---
+++ maxcalc/src/maxcalc.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : maxcalc.cpp
+// Author      : Michael Maximov
+// Version     :
+// Copyright   : (c) 2008 Michael Maximov
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World!!!" << endl; // prints Hello World!!!
+	return 0;
+}
